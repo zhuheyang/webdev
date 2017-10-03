18cb0c71aa006ab07dc2e0d7d9b89d1f256bc633
linjhongwun <271063884@qq.com>
2017-10-03 18:39:45 +0800

    nodejs中的TCP部分

---
+++ 5Node.test/TCP/index.js
@@ -0,0 +1,75 @@
+/* 模块依赖 module_dependencies */
+
+var net = require('net');
+
+  /* 追踪连接数 chase the connect count */
+  /* users是状态变量来的 */
+  var count = 0
+  , users = {};
+
+
+/* 创建服务器 create server */
+/* createServer 回调函数在每次有新连接建立时都会执行
+它会接收一个对象--流(stream), 同时它传递的是net.stream,
+该对象通常可读又可写. */
+var server = net.createServer(function (conn) {
+  var nickname;
+  conn.setEncoding('utf8');  
+
+  conn.write(
+    '\n > welcome to \033[92mnode-chat\033[39m!'
+  + '\n > ' + count + ' other people are connected at this time.' 
+  + '\n > please write your name and press enter: '
+  );
+  count++;
+
+  function broadcast (msg, exceptMyself) {
+    for (var i in users) {
+      if(!exceptMyself || i != nickname) {
+        users[i].write(msg);
+      }
+    }
+  }
+
+  conn.on('data', function (data) {
+    data = data.replace('\r\n', '');
+    
+    // console.log(data);
+    // Eg:接收到的第一份数据应当是用户输入的昵称!
+
+    if (!nickname) {
+      if (users[data]) {
+        conn.write('\033[93m > nickname already in use. try again: \033[39m');
+        return;
+      } else {
+        nickname = data;
+        users[nickname] = conn;
+        
+        for (var i in users) {
+          broadcast('\033[90m > ' + nickname + ' joined the room\033[39m\n');
+        }
+      }
+    } else {
+      //否则视为聊天信息
+      //这里使用了 i != nickname 确保消息发送给出了自己以外的其他客户端
+      for (var i in users) {
+        if (i != nickname) {
+          broadcast('\033[96m > ' + nickname + ':\033[39m ' + data + '\n');
+        }
+      }
+    }
+  });
+  
+  conn.on('close', function () {
+    count--;
+    delete users[nickname];
+    broadcast('\033[90m > ' + nickname + 'left the room\033[39m\n');
+  });
+});
+
+/* 监听 watch */
+
+server.listen(3000, function() {
+  console.log('\033[96m    server listening on *:3000\033[39m');
+});
+
